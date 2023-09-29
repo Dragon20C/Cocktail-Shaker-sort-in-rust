5e3dbd2ff372d5ec327dc047fd28ad558db1b47b
Dragon20C <dragon212c@gmail.com>
2023-09-29 09:43:38 +0100

    Update main.rs made it more efficent.
    reduced iterations inside the for loops by keeping track of the start and end index, so we only for loop the middle values since when we first loop the largest number is set to the right and the smallest number is set to the left in the second for loop.

--- main.rs
+++ main.rs
@@ -1,39 +1,43 @@
 fn cocktail_shaker_sort(arr: &mut [i32]) {
     let length = arr.len();
     let mut swapped;
+    let mut start = 0;
+    let mut end = length - 1;
 
     loop {
         swapped = false;
 
-        for i in 0..length - 1 {
+        // Forward pass
+        for i in start..end {
             if arr[i] > arr[i + 1] {
                 arr.swap(i, i + 1);
                 swapped = true;
             }
         }
 
-        if !swapped {
+        end -= 1;
+
+        if !swapped || start > end {
             break;
         }
 
         swapped = false;
 
-        for i in (0..length - 2).rev() {
+        // Backward pass
+        for i in (start..end).rev() {
             if arr[i] > arr[i + 1] {
-                
                 arr.swap(i, i + 1);
                 swapped = true;
             }
         }
-
-        if !swapped {
+        start += 1;
+        if !swapped || start > end {
             break;
         }
     }
 }
 
 fn main() {
-    let _arr = vec![64, 34, 25, 12, 22, 11, 90,1];
     let mut array = vec![64, 34, 25, 12, 22, 11, 90,1];
     cocktail_shaker_sort(&mut array);
     println!("Sorted array: {:?}", array);
